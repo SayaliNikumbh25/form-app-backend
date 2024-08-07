4fb7b507f2c33f2ffe4b8305c962bcc61b277d1c
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-08-07 13:40:35 +0530

    initial commit

---
+++ routes/forms.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import verifyToken from '../middlewares/verifyToken.js'
+import forms from '../controllers/forms.js';
+
+const router = express.Router();
+
+
+router.get('/',forms.getForms)
+
+router.delete('/delete/:formId', forms.deleteForm);
+
+router.post('/create', forms.createForm);
+
+router.put('/update/:formId', forms.updateForm);
+
+router.get('/public/:id', forms.getOneForm);
+
+router.post('/submit/:id', forms.submitForm);
+
+router.get('/submissions/:id',forms.getSubmittedForm);
+
+export default router
